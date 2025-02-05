6c3906d48fd2e977e1f5958dca05469bade11b26
l3miage-monneroj <joran.monneron@etu.univ-grenoble-alpes.fr>
2025-02-05 16:42:55 +0100

    ajout de view-order component

--- grenoble-express/src/app/app.component.ts
+++ grenoble-express/src/app/app.component.ts
@@ -2,13 +2,15 @@ import {Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CreateTourComponent } from "./create-tour/create-tour.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { ViewOrderComponent } from "./view-order/view-order.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     //RouterOutlet,
     CreateTourComponent,
-    NavBarComponent
+    NavBarComponent,
+    ViewOrderComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
