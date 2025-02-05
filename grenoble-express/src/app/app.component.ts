d7c0354e2f6adb5ce8aa1e6e2b8332c54f47bf87
l3miage-monneroj <joran.monneron@etu.univ-grenoble-alpes.fr>
2025-02-05 13:42:54 +0100

    Component navbar

--- grenoble-express/src/app/app.component.ts
+++ grenoble-express/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CreateTourComponent } from "./create-tour/create-tour.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     //RouterOutlet,
-    CreateTourComponent
+    CreateTourComponent,
+    NavBarComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
