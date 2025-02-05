a6827bf6f7baf342307fa71edaf1c8269607d364
l3miage-monneroj <joran.monneron@etu.univ-grenoble-alpes.fr>
2025-02-05 08:47:33 +0100

    Version1 de l'nterface, rasolont et monneroj

--- grenoble-express/src/app/app.component.ts
+++ grenoble-express/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import {Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreateTourComponent } from "./create-tour/create-tour.component";
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
-  ],
+    //RouterOutlet,
+    CreateTourComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ grenoble-express/src/app/create-tour/create-tour.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+/*@Component({
+  selector: 'app-create-tour',
+  imports: [],
+  templateUrl: './create-tour.component.html',
+  styleUrl: './create-tour.component.scss'
+})
+export class CreateTourComponent {
+
+}/*
+/**
+ * @title Basic buttons
+ */
+
+
+interface Livreur {
+  name: string;
+}
+interface Camion{
+  code: string;
+}
+
+@Component({
+  selector: 'app-create-tour',
+  templateUrl: './create-tour.component.html',
+  styleUrl: './create-tour.component.scss',
+  imports: [MatButtonModule, MatDividerModule, MatIconModule,FormsModule,MatButtonModule,MatInputModule,MatSelectModule,MatFormFieldModule],
+})
+
+export class CreateTourComponent {
+  //valeur possible de livreur a selectionner 
+  Livreurs: Livreur[] = [
+    {name: 'Jojo'},
+    {name : 'Tiana'},
+    {name: 'Fatimouta'},
+  ];
+
+  //valeur possible de camion a selectionner 
+  Camions: Camion[]=[
+    {code: 'cg135'},
+    {code: 'cg137'},
+    {code: 'cg735'},
+  ]
+}
